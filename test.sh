763dff6a8861312b29b1ac8cf8c78050ebd46316
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-12-03 23:55:05 +0000

    trying to load kernel to memory

--- test.sh
+++ test.sh
@@ -4,3 +4,9 @@ qemu-system-x86_64 -fda main.bin -display gtk -serial stdio
 qemu-system-x86_64 -fda main.bin -vnc :0 -serial stdio
 
 
+qemu-system-x86_64 -fda f.img -display gtk -serial stdio
+
+
+
+sudo dd if=main.bin of=f.img bs=512 conv=notrunc
+sudo dd if=kernel/main.bin of=f.img seek=1 bs=512 conv=notrunc
