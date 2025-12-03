19a478d621d55e4c331633edccfe6758e191fd9c
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-12-03 21:37:22 +0000

    issues

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 nasm -f bin main.S -o main.bin
 qemu-system-x86_64 -fda main.bin  -nographic
+qemu-system-x86_64 -fda main.bin -display gtk -serial stdio
+qemu-system-x86_64 -fda main.bin -vnc :0 -serial stdio
+
 
