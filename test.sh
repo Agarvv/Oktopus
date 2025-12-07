d01672b29d5ffbf6a7255037d29ea5c21a1da3e9
Agarvv <casluagarv@gmail.com>
2025-12-07 15:38:31 +0100

    issues

--- test.sh
+++ test.sh
@@ -10,3 +10,7 @@ qemu-system-x86_64 -fda f.img -display gtk -serial stdio
 
 sudo dd if=main.bin of=f.img bs=512 conv=notrunc
 sudo dd if=kernel/main.bin of=f.img seek=1 bs=512 conv=notrunc
+
+qemu-system-x86_64 -hda f.img -display gtk -serial stdio
+
+qemu-system-x86_64 -drive file=f.img,format=raw,if=ide -display gtk -serial stdio
