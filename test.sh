14a68fc67f80e0f0626abb77101a3d7e53176e26
Agarvv <casluagarv@gmail.com>
2025-12-10 13:21:31 +0100

    c

--- test.sh
+++ test.sh
@@ -11,8 +11,6 @@ qemu-system-x86_64 -fda f.img -display gtk -serial stdio
 sudo dd if=main.bin of=f.img bs=512 conv=notrunc
 sudo dd if=kernel/main.bin of=f.img seek=1 bs=512 conv=notrunc
 
-qemu-system-x86_64 -hda f.img -display gtk -serial stdio
+qemu-system-x86_64 -hda f.img -nographic -serial stdio
 
 qemu-system-x86_64 -drive file=f.img,format=raw,if=ide -display gtk -serial stdio
-
-qemu-system-x86_64 -hda f.img -nographic -serial mon:stdio
