0d0006559ceb95b939a6f3020c875b714129947e
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-12-10 12:45:09 +0000

    issues

--- img.sh
+++ img.sh
@@ -2,4 +2,4 @@ nasm -f bin main.S -o main.bin
 rm f.img 
 touch f.img 
 sudo dd if=main.bin of=f.img bs=512 conv=notrunc
-sudo dd if=kernel/main.bin of=f.img seek=1 bs=512 conv=notrunc
+sudo dd if=kernel/kernel.bin of=f.img seek=1 bs=512 conv=notrunc
