983b5accac8f20d041490414f4d96f941deff74a
Agarvv <casluagarv@gmail.com>
2025-12-14 15:48:07 +0100

    cleaning

--- img.sh
+++ img.sh
@@ -1,12 +1,15 @@
-chmod +x kernel/comp.sh
+chmod +x src/kernel/comp.sh
 chmod +x img.sh
 
+
 echo "Bootloader Compilation..."
-nasm -f bin bootloader/main.S -o bootloader/main.bin 
+nasm -f bin src/bootloader/main.S -o  src/bootloader/main.bin 
 
 echo "Kernel Compilation..."
+cd src 
 cd kernel
 ./comp.sh 
+cd .. 
 cd .. 
 
 echo "Compilation OK"
@@ -14,7 +17,7 @@ echo "Compilation OK"
 echo "Img Creation..."
 rm f.img 
 touch f.img 
-sudo dd if=bootloader/main.bin of=f.img bs=512 conv=notrunc
-sudo dd if=kernel/main/kernel.bin of=f.img seek=1 bs=512 conv=notrunc
+sudo dd if=src/bootloader/main.bin of=f.img bs=512 conv=notrunc
+sudo dd if=src/kernel/main/kernel.bin of=f.img seek=1 bs=512 conv=notrunc
 
 echo "Done!"
