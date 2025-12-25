e205ca846a62b33c4223ef22f799dae182ed1d1f
Agarvv <casluagarv@gmail.com>
2025-12-25 02:12:24 +0100

    long mode

--- src/kernel/main/longmode.c
+++ src/kernel/main/longmode.c
@@ -2,7 +2,17 @@
 #include <kernel/main/paging.h>
 
 void enable_physical_addr_extension() {
- 
+ // to enable PAE, i have to set bit 5 of CR4 register to 1. 
+ asm volatile(
+		 "
+  push eax 
+  mov eax, cr4
+  00000000 00000000 00000000 00A000000 
+  OR eax, 0x00000020
+  mov cr4, eax
+  pop eax
+ "
+ ); 
 }
 
 void enable_long_mode() {
