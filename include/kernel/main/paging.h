e748c95bf1f3eccb10faac292d696efd1c9ed66d
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-12-26 23:00:03 +0000

    long mode

--- include/kernel/main/paging.h
+++ include/kernel/main/paging.h
@@ -1,4 +1,4 @@
-unsigned long long page_map_level_four[512] __attribute__((aligned(4096)));
+unsigned int page_map_level_four[1024] __attribute__((aligned(4096)));
 // Bit 0: Present
 // Bit 1: Read/Write
 // Bit 2: User/Supervisor
@@ -6,12 +6,12 @@ unsigned long long page_map_level_four[512] __attribute__((aligned(4096)));
 // Bit 4: Page Cache Disable
 // Bit 5: Accessed
 // Bits 6-11: Ignored / Available to OS
-// Bits 12-51: Physical address of PDPT
-// Bits 52-62: Available to OS
-// Bit 63: NX (No Execute)
-// 0 00000000000 000000000000000000000000000000000000 000000 000011
+// Bits 12-31: Physical address of PDPT
+// Bits 32-31: Available to OS
+// Bit 31: NX (No Execute)
+// 0 00000000000 00000000000000000000 000000 000011
 
-unsigned long long page_directory_pointer_table[512] __attribute__((aligned(4096)));
+unsigned int page_directory_pointer_table[1024] __attribute__((aligned(4096)));
 // Bit 0: Present
 // Bit 1: Read/Write
 // Bit 2: User/Supervisor
@@ -21,12 +21,12 @@ unsigned long long page_directory_pointer_table[512] __attribute__((aligned(4096
 // Bit 6: Dirty (for large pages)
 // Bit 7: Page Size (1=1GB if supported)
 // Bits 8-11: Ignored / Available to OS
-// Bits 12-51: Physical address of PD
-// Bits 52-62: Available to OS
-// Bit 63: NX (No Execute)
-//  0 0000000 000000000000000000000000000000000010 0000 00000011
+// Bits 12-31: Physical address of PD
+// Bits 32-31: Available to OS
+// Bit 31: NX (No Execute)
+//  0 0000000 00000000000000000000 0000 00000011
 
-unsigned long long page_directory[512] __attribute__((aligned(4096)));
+unsigned int page_directory[1024] __attribute__((aligned(4096)));
 // Bit 0: Present
 // Bit 1: Read/Write
 // Bit 2: User/Supervisor
@@ -36,12 +36,12 @@ unsigned long long page_directory[512] __attribute__((aligned(4096)));
 // Bit 6: Dirty (for large pages)
 // Bit 7: Page Size (1=2MB if supported)
 // Bits 8-11: Ignored / Available to OS
-// Bits 12-51: Physical address of PT or large page
-// Bits 52-62: Available to OS
-// Bit 63: NX (No Execute)
-//  0 0000000 000000000000000000000000000000000011 0000 00000011
+// Bits 12-31: Physical address of PT or large page
+// Bits 32-31: Available to OS
+// Bit 31: NX (No Execute)
+//  0 0000000 00000000000000000000 0000 00000011
 
-unsigned long long page_table[512] __attribute__((aligned(4096)));
+unsigned int page_table[1024] __attribute__((aligned(4096)));
 // Bit 0: Present
 // Bit 1: Read/Write
 // Bit 2: User/Supervisor
@@ -50,11 +50,10 @@ unsigned long long page_table[512] __attribute__((aligned(4096)));
 // Bit 5: Accessed
 // Bit 6: Dirty
 // Bits 7-11: Ignored / Available to OS
-// Bits 12-51: Physical address of 4 KB page
-// Bits 52-62: Available to OS
-// Bit 63: NX (No Execute)
-// 0 00000 000000000000000000000000000000000100 00000 000011
+// Bits 12-31: Physical address of 4 KB page
+// Bits 32-31: Available to OS
+// Bit 31: NX (No Execute)
+// 0 00000 00000000000000000000 00000 000011
 
 
 void enable_four_level_paging();
-
--- src/kernel/main/longmode.c
+++ src/kernel/main/longmode.c
@@ -3,6 +3,16 @@
 
 
 void enable_physical_addr_extension() {
+  unsigned int pd_addr = (unsigned int)&page_directory << 12; 
+	page_directory_pointer_table[0] = 0x00000001 | pd_addr;
+  
+  unsigned int pt_addr = (unsigned int)&page_table << 12; 
+	page_directory[0] = 0x00000001 | pt_addr;
+  
+	unsigned int p_addr = 0x00010000; 
+	page_table[0] = 0x00000001 | p_addr; 
+  
+
   // to enable PAE, i have to set bit 5 of CR4 register to 1. 
     asm volatile(
         "movl %%cr4, %%eax\n\t"
@@ -11,6 +21,7 @@ void enable_physical_addr_extension() {
         ::: "eax", "memory"
     );
 }
+
 
 void enable_long_mode() {
   // in order to activate long mode, i have to set bit eigth (Long mode enable) of EFER msr to 1
@@ -26,7 +37,7 @@ void enable_long_mode() {
 }
 
 void long_mode_start() {
-  enable_four_level_paging(); 
   enable_physical_addr_extension(); 
+  enable_four_level_paging(); 
   enable_long_mode(); 
 }
